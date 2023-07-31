c1682c38f169fd7fcc12b54a56f2a2ee5c1b5112
vincent owino <owinovincent589@gmail.com>
2023-07-31 09:38:18 +0300

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,17 @@
+
 function Hero (){
     return(
-<div>vincent</div>
+<div className=" bg-[url('assets/images/max.jpg')] bg-no-repeat bg-center h-[70vh] w-[100vw] relative">
+    <div className=" absolute translate-y-52 translate-x-36" >
+<p className=" text-5xl w-[50%]  text-white font-bold mb-8">Digitally Find and Hire Exceptional Artisans and Builders
+</p>
+<div className=" flex justify-start gap-4">
+<button  className="border-2 rounded-md border-orange-500 text-center bg-orange-500 py-4 px-4 text-2xl">Find A Technician</button>
+<h2 className="mt-5 text-2xl">Or</h2>
+<button className="border-2 rounded-md border-orange-500 text-center bg-orange-500 py-4 px-4 text-2xl">Join As A Technician</button>
+</div>
+</div>
+</div>
     )
 }
 export default Hero
---
+++ src/components/More.jsx
@@ -0,0 +1,8 @@
+function More (){
+    return(
+        <div className="m-[2em] flex justify-center   ">
+          <button className="rounded-md border-2 bg-orange-500 text-[2rem] text-center py-4 px-4">Show More</button>  
+        </div>
+    )
+}
+export default More
